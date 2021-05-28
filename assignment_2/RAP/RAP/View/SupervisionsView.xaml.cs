7af8758d63e7d19b1f2132f16b6930cd4042278d
ZhenliangWu513 <zwu11@utas.edu.au>
2021-05-29 02:11:15 +1000

    views: PublicationDetailsView, CulmativeCountView, SupervisionsView

---
+++ assignment_2/RAP/RAP/View/SupervisionsView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RAP.View
+{
+    /// <summary>
+    /// SupervisionsView.xaml 的交互逻辑
+    /// </summary>
+    public partial class SupervisionsView : Window
+    {
+        public SupervisionsView()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
